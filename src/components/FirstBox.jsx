1448a502ad6ba2cd40cc9b3d9b81876dc2fb5403
Fred <huertaserick@yahoo.ie>
2024-12-05 19:19:40 +0000

    Just realise purple and violet not the samegit add .

--- src/components/FirstBox.jsx
+++ src/components/FirstBox.jsx
@@ -1,10 +1,17 @@
 const FirstBox = () => {
   return (
-    <div className="grid grid-cols-3 grid-rows-2  gap-4 border-2 border-red-600">
-      <div className="col-span-1 border-2 border-blue-300">
-        <h1>Social Media 10x Faster with AI</h1>
-        <img src="/illustration-five-stars.webp" alt="Five Stars" />
-        Over 4,000 5-star reviews
+    <div className="grid place-items-center grid-cols-3 grid-rows-2 p-3 gap-4 border-2 border-red-600">
+      <div className="col-span-1 bg-violet-600 text-center rounded-lg place-items-center p-5">
+        <h1 className="text-6xl mb-5 mt-10 text-white text-center">
+          Social Media <span className="text-orange-200">10x</span>{" "}
+          <i>Faster</i> with AI
+        </h1>
+        <img
+          className="w-60 h-10"
+          src="/illustration-five-stars.webp"
+          alt="Five Stars"
+        />
+        <p className="text-white mt-2 mb-10">Over 4,000 5-star reviews</p>
       </div>
 
       <div className="grid grid-cols-2 col-span-2 border-2 border-blue-700">
